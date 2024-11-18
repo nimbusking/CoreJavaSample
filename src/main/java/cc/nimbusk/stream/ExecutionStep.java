837ca3cd3069c6a693c5505b8245725716a8e80d
nimbusking <kemivong@hotmail.com>
2024-11-18 08:27:31 +0800

    补充示例

--- src/main/java/cc/nimbusk/stream/ExecutionStep.java
+++ src/main/java/cc/nimbusk/stream/ExecutionStep.java
@@ -3,7 +3,10 @@ package cc.nimbusk.stream;
 import cc.nimbusk.stream.bean.Student;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 一个debug程序来剖析一下一个Stream执行的整体流程
@@ -22,6 +25,17 @@ public class ExecutionStep {
                 .max()
                 .ifPresent(System.out::println);
 
+
+        // 配套生成
+        List<Long> idList = Arrays.asList(10L, 22L, 22L, 33L, 1L, 4L, 10L, 4L, 100L, 200L, 300L, 102L, 20L, 30L, 11L);
+        List<Student> debugList = idList.stream()
+                .distinct()
+                .map(id -> new Student(id, "name" + id, null, 20, 0))
+                .filter(student -> student.getId() > 10)
+                .filter(student -> student.getId() < 100)
+                .sorted(Comparator.comparing(Student::getId))
+                .collect(Collectors.toList());
+        log.info("After test students list:{}", debugList);
     }
 
 }
--- src/main/java/cc/nimbusk/stream/bean/Student.java
+++ src/main/java/cc/nimbusk/stream/bean/Student.java
@@ -1,8 +1,12 @@
 package cc.nimbusk.stream.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private Long id;
     private String name;
